0acacf495a6166e18cbd97fa953ad0a19e09dbce
전상효 <junsanghyo0820@gmail.com>
2021-09-26 15:08:53 +0900

    전화걸기(CALL) 활용 -> 권한이 없어서 앱이 죽는다.

--- app/src/main/java/com/jayshe/librarypractice_20210926/MainActivity.kt
+++ app/src/main/java/com/jayshe/librarypractice_20210926/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.jayshe.librarypractice_20210926
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,6 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        callBtn.setOnClickListener { 
+            
+//            Intent(4) 바로 전화 연결 -> CALL 액션
+
+            val myUri = Uri.parse("Tel : 010-5555-6666")
+            val myIntent = Intent( Intent.ACTION_CALL, myUri)
+            startActivity(myIntent)
+
+
+        }
+        
         profileImg.setOnClickListener {
 
 //
